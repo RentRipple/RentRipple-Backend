968692fc4de6174ff2895e38e61bb5f67a4eb86f
khushbu <0109khushbu@gmail.com>
2024-06-10 23:44:30 -0400

    Revert "fix: change password regex for validation"
    This reverts commit dc171fd52d1b23386cd59a0c2fc98afa4f03dbe9.

--- src/Helpers/validationSchema.ts
+++ src/Helpers/validationSchema.ts
@@ -19,9 +19,7 @@ export const registerationSchema = Joi.object({
   email: Joi.string().email().lowercase().required(),
   password: Joi.string()
     .min(8)
-    .pattern(new RegExp(
-      "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
-    ))
+    .pattern(new RegExp("^[a-zA-Z0-9]{8,}$"))
     .required(),
   securityQuestions: Joi.array()
     .items(securityQuestionSchema)
@@ -40,8 +38,6 @@ export const loginSchema = Joi.object({
 export const newPasswordSchema = Joi.object({
   password: Joi.string()
     .min(8)
-    .pattern(new RegExp(
-      "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
-    ))
+    .pattern(new RegExp("^[a-zA-Z0-9]{8,}$"))
     .required(),
 });
